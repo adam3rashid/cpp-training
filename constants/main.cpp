30900f127acf600b3115bbd83f55c73d6e0ff068
Adam Rashid <rashidadamjee879@gmail.com>
2024-02-28 11:45:47 +0300

    constants

---
+++ constants/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    const int age {25};
+    const float height {5.6f};
+    
+    //You cannot declare const without initialization, it will throw an error
+    //const int age; this line throws an error
+
+    // age = 29; the variable age cannot be changed due to its datatypes, it cannot b be modified
+    // height = 9.2; throws an error even before compiling same as variable age; it can't be modified
+    std::cout << "Age is: " <<  age << std::endl;
+    std::cout << "Height is: " << height << std::endl;
+    return 0;
+}
