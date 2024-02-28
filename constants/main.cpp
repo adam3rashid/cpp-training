5ec26c43aa70597f11345b14dce7b7fe76ee24ee
Adam Rashid <rashidadamjee879@gmail.com>
2024-02-28 11:47:15 +0300

    Involving constants in operations

--- constants/main.cpp
+++ constants/main.cpp
@@ -3,6 +3,7 @@
 int main(){
     const int age {25};
     const float height {5.6f};
+    int years {3 * age};
     
     //You cannot declare const without initialization, it will throw an error
     //const int age; this line throws an error
@@ -11,5 +12,6 @@ int main(){
     // height = 9.2; throws an error even before compiling same as variable age; it can't be modified
     std::cout << "Age is: " <<  age << std::endl;
     std::cout << "Height is: " << height << std::endl;
+    std::cout << "Years is: " << years << std::endl;
     return 0;
 }
